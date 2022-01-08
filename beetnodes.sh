136f359326a8258800054dad1c09b35a62885c29
gemchox <86078187+gemchox@users.noreply.github.com>
2022-01-08 11:56:02 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -2,13 +2,13 @@
 # Este es el script de inicio de BeetNodes! Made by HiroBeet! 🦑
 rm -r mnt/server/* &> /dev/null ### Eliminando todos los archivos actuales!
 #/////////////// 🌵 Funciones (1) 🌵 ///////////////#
-function a {
+function ae {
  echo 'a'
 }
 
 #if [-f /home/container/.custom.beetnodes ]; then
-    echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Felicidades por seleccionar la instalacion \e[4m\e[95mcustom\e[39m\e[0m!'
+    #echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Felicidades por seleccionar la instalacion \e[4m\e[95mcustom\e[39m\e[0m!'
 #else
 #    if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/${ARCHIVO_INICIO}
 #fi
-a
+ae
