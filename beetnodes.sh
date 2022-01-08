48ce17763c21ba65f8f2240dfa47ef1d45618f4b
gemchox <86078187+gemchox@users.noreply.github.com>
2022-01-08 15:15:03 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -9,21 +9,41 @@ FILE_ALCATRAZ=.alcatraz.beetnodes
 
 #/////////////// 🌵 Funciones (1) 🌵 ///////////////#
 
+function bienvenida { ### Funcion de bienvenida!
+        sleep 0.5
+            clear
+        sleep 1
+    echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Bienvenid@ a BeetNodes, gracias por confiar en \e[4m\e[95mBeetNodes\e[39m\e[0m!'
+        sleep 1
+    echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] En \e[4m\e[95mBeetNodes\e[39m\e[0m estamos muy felices de verte aqui, y esperamos que disfrutes de los servicios que ofrecemos, de alta calidad, por un muy bajo costo!'
+        sleep 1.5
+            clear
+}
+
 function check_custom { ### Check Custom (INSTALACION CUSTOM)
     if [ -f "$FILE_CUSTOM" ]; then
-        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Bienvenid@ a BeetNodes, has seleccionado una instalacion \e[4m\e[95mCustom\e[39m\e[0m!'
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado una instalacion \e[4m\e[95mCustom\e[39m\e[0m!'
+            
     elif [ -f "$FILE_TANJIRO" ]; then
-        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Bienvenid@ a BeetNodes, has seleccionado a \e[4m\e[95mTanjiro\e[39m\e[0m!'
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y seleccionado a \e[4m\e[95mTanjiro\e[39m\e[0m!'
+            sleep 1.5
+                    echo
+            sleep 0.5
+                clear
+        echo -r '\e[92m* \e[39m[\e[94mINFO\e[39m] Iniciando instalacion...'
             if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/index.js
     elif [ -f "$FILE_ALCATRAZ" ]; then
-        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Bienvenid@ a BeetNodes, has seleccionado a \e[4m\e[95mAlcatraz\e[39m\e[0m!'
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y seleccionado a \e[4m\e[95mAlcatraz\e[39m\e[0m!'
+            sleep 1.5
+                    echo
+            sleep 0.5
+                clear
+        echo -r '\e[92m* \e[39m[\e[94mINFO\e[39m] Iniciando instalacion...'
             if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/index.js
     fi
 }
 
 #/////////////// 🎂 Inicio del script 🎂 ///////////////#
 
-sleep 0.5
-clear
-sleep 1
-check_custom
+bienvenida # Llamando bienvenida!
+check_custom # Llamando check_custom!
