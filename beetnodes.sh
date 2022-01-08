6c2f5e5270eac4161f0935bd1cea9a9dfea617f8
gemchox <86078187+gemchox@users.noreply.github.com>
2022-01-08 15:22:57 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -14,16 +14,16 @@ function bienvenida { ### Funcion de bienvenida!
             clear
         sleep 1
     echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Bienvenid@ a BeetNodes, gracias por confiar en \e[4m\e[95mBeetNodes\e[39m\e[0m!'
-        sleep 1
+        sleep 3
     echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] En \e[4m\e[95mBeetNodes\e[39m\e[0m estamos muy felices de verte aqui, y esperamos que disfrutes de los servicios que ofrecemos, de alta calidad, por un muy bajo costo!'
-        sleep 1.5
+        sleep 10
             clear
 }
 
 function check_custom { ### Check Custom (INSTALACION CUSTOM)
     if [ -f "$FILE_CUSTOM" ]; then
         echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado una instalacion \e[4m\e[95mCustom\e[39m\e[0m!'
-            
+
     elif [ -f "$FILE_TANJIRO" ]; then
         echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y seleccionado a \e[4m\e[95mTanjiro\e[39m\e[0m!'
             sleep 1.5
