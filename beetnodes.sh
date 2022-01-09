1920e8a7a245ac8d63948f95ffd26a2169487d78
gemchox <86078187+gemchox@users.noreply.github.com>
2022-01-09 18:36:40 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -8,7 +8,7 @@ FILE_CUSTOM=.custom.beetnodes
 FILE_TANJIRO=.tanjiro.beetnodes
 FILE_ALCATRAZ=.alcatraz.beetnodes
 FILE_DOGEBOT=.dogebot.beetnodes
-FILE_DOGEBOT_V2=.dogebot.v2.beetnodes
+FILE_ESUBOT=.esubot.beetnodes
 FILE_START_MAINJS=.start.mainjs.beetnodes
 FILE_START_INDEXJS=.start.indexjs.beetnodes
 
@@ -117,8 +117,8 @@ function check_instalacion { ### Check instalacion
             sleep 2.5
                 echo
                     if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/main.js
-    elif [ -f "$FILE_DOGEBOT_V2" ]; then
-        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y seleccionado a \e[4m\e[95mDogeBot-V2\e[39m\e[0m!'
+    elif [ -f "$FILE_ESUBOT" ]; then
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y seleccionado a \e[4m\e[95mEsuBot\e[39m\e[0m!'
             sleep 3
                     echo
             sleep 2
