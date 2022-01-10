f50c2ac3dad697e8dff974daa32cdc199c6370ff
gemchox <86078187+gemchox@users.noreply.github.com>
2022-01-10 12:16:55 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -8,7 +8,7 @@ FILE_CUSTOM=.custom.beetnodes
 FILE_TANJIRO=.tanjiro.beetnodes
 FILE_ALCATRAZ=.alcatraz.beetnodes
 FILE_DOGEBOT=.dogebot.beetnodes
-# FILE_ESUBOT=.esubot.beetnodes
+FILE_FX_BOT_V83=.fxbot.v83.beetnodes
 FILE_START_MAINJS=.start.mainjs.beetnodes
 FILE_START_INDEXJS=.start.indexjs.beetnodes
 
@@ -117,9 +117,8 @@ function check_instalacion { ### Check instalacion
             sleep 2.5
                 echo
                     if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/main.js
-    """
-    elif [ -f "$FILE_ESUBOT" ]; then
-        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mEsuBot\e[39m\e[0m!'
+    elif [ -f "$FILE_FX_BOT_V83" ]; then
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mFx-Bot-V83\e[39m\e[0m!'
             sleep 3
                     echo
             sleep 2
@@ -128,7 +127,6 @@ function check_instalacion { ### Check instalacion
             sleep 2.5
                 echo
                     if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/index.js
-    """
     else
         sleep 2
                 clear
