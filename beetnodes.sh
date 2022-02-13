0efe9d090635ccd6d416713d346c50a8e69e6d30
gemchox <86078187+gemchox@users.noreply.github.com>
2022-02-13 18:50:30 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -11,7 +11,7 @@ FILE_DOGEBOT=.dogebot.beetnodes
 FILE_NK_BOT=.nkbot.beetnodes
 FILE_SHADOW_BOT_V3=.shadowbot.v3.beetnodes
 FILE_FXBOT_V83_2=.fxbot.v83-2.beetnodes
-FILE_NYAN_BOT=.nyanbot.beetnodes
+FILE_DYLUX_BOT=.dyluxbot.beetnodes
 FILE_CUSTOM=.custom.beetnodes
 FILE_START_MAINJS=.start.mainjs.beetnodes
 FILE_START_INDEXJS=.start.indexjs.beetnodes
@@ -196,8 +196,8 @@ function check_instalacion { ### Check instalacion
             sleep 2.5
                 echo
                     if [ -f /home/container/package.json ]; then /usr/local/bin/node /home/container/index.js; fi;
-    elif [ -f "$FILE_NYAN_BOT" ]; then
-        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mNyanBot\e[39m\e[0m!'
+    elif [ -f "$FILE_DYLUX_BOT" ]; then
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mDylux-Bot\e[39m\e[0m!'
             sleep 3
                     echo
             sleep 2
@@ -205,12 +205,7 @@ function check_instalacion { ### Check instalacion
         echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Iniciando servidor...'
             sleep 2.5
                 echo
-                    /usr/local/bin/npm install node-tesseract-ocr;
-                    /usr/local/bin/npm install @adiwajshing/baileys;
-                    /usr/local/bin/npm install ffmpeg;
-                    /usr/local/bin/npm install cfonts;
-                    /usr/local/bin/npm update;
-                        if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/samu.js
+                        if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/main.js
     else
         sleep 2
                 clear
