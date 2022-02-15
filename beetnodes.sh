b65d1699e7125f36f1c1c1f628f28abf43f51c86
gemchox <86078187+gemchox@users.noreply.github.com>
2022-02-15 21:24:46 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -17,6 +17,7 @@ FILE_GAMES_WABOT=.games.wabot.beetnodes
 FILE_TERMUX_WABOT_ECONOMY=.termux.wabot.economy.beetnodes
 FILE_BABY_BECHOSO_BOT=.baby.bechoso.beetnodes
 FILE_FGBOT_BOT_V4=.fbbot.v4.beetnodes
+FILE_TURBO_BOT_V3=.turbo.v3.beetnodes
 FILE_CUSTOM=.custom.beetnodes
 FILE_START_MAINJS=.start.mainjs.beetnodes
 FILE_START_INDEXJS=.start.indexjs.beetnodes
@@ -256,6 +257,16 @@ function check_instalacion { ### Check instalacion
                         if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/thechoute.js
     elif [ -f "$FILE_FGBOT_BOT_V4" ]; then
         echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mFgBotV4\e[39m\e[0m!'
+            sleep 3
+                    echo
+            sleep 2
+                clear
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Iniciando servidor...'
+            sleep 2.5
+                echo
+                        if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/main.js
+    elif [ -f "$FILE_TURBO_BOT_V3" ]; then
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mTurboBot-V3\e[39m\e[0m!'
             sleep 3
                     echo
             sleep 2
