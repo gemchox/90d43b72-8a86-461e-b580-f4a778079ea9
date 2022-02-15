ab15208325806488a927b5320f54a4e8cb6f63af
gemchox <86078187+gemchox@users.noreply.github.com>
2022-02-15 21:06:01 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -190,7 +190,7 @@ function check_instalacion { ### Check instalacion
         echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Iniciando servidor...'
             sleep 2.5
                 echo
-                    if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/index.js
+                    if [ -f /home/container/node_modules ]; then /usr/local/bin/node /home/container/index.js; fi;
     elif [ -f "$FILE_FXBOT_V83_2" ]; then
         echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mFxBotV83.2\e[39m\e[0m!'
             sleep 3
