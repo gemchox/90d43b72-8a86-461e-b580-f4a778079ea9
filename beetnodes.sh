92c7add3d65667746978e60398f866ac6f73cfbb
gemchox <86078187+gemchox@users.noreply.github.com>
2022-02-15 18:02:06 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -14,6 +14,7 @@ FILE_FXBOT_V83_2=.fxbot.v83-2.beetnodes
 FILE_DYLUX_BOT=.dyluxbot.beetnodes
 FILE_WABOT_AQ=.wabot.aq.beetnodes
 FILE_GAMES_WABOT=.games.wabot.beetnodes
+FILE_TERMUX_WABOT_ECONOMY=.termux.wabot.economy.beetnodes
 FILE_CUSTOM=.custom.beetnodes
 FILE_START_MAINJS=.start.mainjs.beetnodes
 FILE_START_INDEXJS=.start.indexjs.beetnodes
@@ -220,6 +221,16 @@ function check_instalacion { ### Check instalacion
                         if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/index.js
     elif [ -f "$FILE_GAMES_WABOT" ]; then
         echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mGames-WaBot\e[39m\e[0m!'
+            sleep 3
+                    echo
+            sleep 2
+                clear
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Iniciando servidor...'
+            sleep 2.5
+                echo
+                        if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/index.js
+    elif [ -f "$FILE_TERMUX_WABOT_ECONOMY" ]; then
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mTermux-WaBot-Economy\e[39m\e[0m!'
             sleep 3
                     echo
             sleep 2
