068249c57d938470064fc3bd086cdfb1c3254882
gemchox <86078187+gemchox@users.noreply.github.com>
2022-02-16 08:36:22 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -7,6 +7,7 @@ FILE_FIRST_START=.first.beetnodes
 FILE_TANJIRO=.tanjiro.beetnodes
 FILE_ALCATRAZ=.alcatraz.beetnodes
 FILE_ALCATRAZ_V2=.alcatraz.v2.beetnodes
+FILE_ASTERIA=.asteria.beetnodes
 FILE_DOGEBOT=.dogebot.beetnodes
 FILE_NK_BOT=.nkbot.beetnodes
 FILE_SHADOW_BOT_V3=.shadowbot.v3.beetnodes
@@ -163,6 +164,16 @@ function check_instalacion { ### Check instalacion
             sleep 2.5
                 echo
                     if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/index.js
+    elif [ -f "$FILE_ASTERIA" ]; then
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mAsteria\e[39m\e[0m!'
+            sleep 3
+                    echo
+            sleep 2
+                clear
+        echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Iniciando servidor...'
+            sleep 2.5
+                echo
+                    if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/main.js
     elif [ -f "$FILE_DOGEBOT" ]; then
         echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mDogeBot\e[39m\e[0m!'
             sleep 3
