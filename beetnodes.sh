b188cd9086dc9a441f6c3532b7e17d90e4663ff4
gemchox <86078187+gemchox@users.noreply.github.com>
2022-02-16 21:06:03 +0000

    Update beetnodes.sh

--- beetnodes.sh
+++ beetnodes.sh
@@ -163,7 +163,7 @@ function check_instalacion { ### Check instalacion
         echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Iniciando servidor...'
             sleep 2.5
                 echo
-                    if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/node /home/container/index.js
+                    if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; /usr/local/bin/npm start
     elif [ -f "$FILE_ASTERIA" ]; then
         echo -e '\e[92m* \e[39m[\e[94mINFO\e[39m] Acabas de iniciar tu servidor, y has seleccionado \e[4m\e[95mAsteria\e[39m\e[0m!'
             sleep 3
